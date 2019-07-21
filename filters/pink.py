29c62bb05ae1f95aae736ce206226d63a7ae0577
Joel Geddert <joeltronics@users.noreply.github.com>
2019-07-21 12:37:09 -0700

    pink filter improvements

--- filters/pink.py
+++ filters/pink.py
@@ -21,32 +21,50 @@ class BasicPinkFilter(IIRFilterBase):
 
 
 class PinkFilter(ParallelProcessors):
-	def __init__(self, sample_rate):
+	def __init__(self, sample_rate, pole_zero_ratio=2.0):
 		self.sample_rate = sample_rate
-		freqs, gains = self._calc_individual_filters(self.sample_rate)
+		freqs, gains = self._calc_individual_filters(self.sample_rate, pole_zero_ratio=pole_zero_ratio)
 		filters = [BasicOnePole(wc=f/sample_rate, gain=g) for f, g in zip(freqs, gains)]
 		super().__init__(filters)
 
 	@staticmethod
-	def _calc_individual_filters(sample_rate, pole_zero_ratio=2.0) -> Tuple[np.ndarray, np.ndarray]:
+	def _calc_individual_filters(sample_rate, pole_zero_ratio, start_freq_Hz=10.0, downward=False) -> Tuple[np.ndarray, np.ndarray]:
 		# http://www.firstpr.com.au/dsp/pink-noise/#Filtering
 		# http://www.cooperbaker.com/home/code/pink%20noise/
 
 		freqs = []
 		gains = []
 
+		pzr_sq = pole_zero_ratio ** 2.0
+
 		gain = 1.0
-		hz = sample_rate / (2 * np.pi)
 
-		while hz > 1:
-			freqs.append(hz)
-			gains.append(gain)
+		if downward:
+			hz = sample_rate / (2 * np.pi)
+			while hz > start_freq_Hz:
+				freqs.append(hz)
+				gains.append(gain)
+				gain *= pole_zero_ratio
+				hz /= pzr_sq
 
-			gain *= pole_zero_ratio
-			hz /= (2.0 * pole_zero_ratio)
+			gain_norm = 1.0 / sum(gains)
+
+		else:
+			hz = start_freq_Hz
+			while hz < (0.5*sample_rate):
+				freqs.append(hz)
+				gains.append(gain)
+				gain /= pole_zero_ratio
+				hz *= pzr_sq
+
+			# FIXME
+			# Figured this gain out empirically - works for 2 and sqrt(2), but nothing else
+			#gain_norm = 0.125 * pzr_sq
+
+			gain_norm = 1.0 / sum(gains)
 
 		freqs = np.array(freqs)
-		gains = np.array(gains) / sum(gains)
+		gains = np.array(gains) * gain_norm
 
 		return freqs, gains
 
@@ -55,8 +73,12 @@ def test(verbose=False):
 	import numpy as np
 	from unit_test.processor_unit_test import ProcessorUnitTest
 	from unit_test.unit_test import run_unit_tests
+	from math import sqrt
+
+	sqrt2 = sqrt(2.0)
 
 	freqs = np.array([50., 100., 200., 400., 800., 1600., 3200., 6400., 12800.]) / 44100.
+
 	expected_vals = -3.0 * np.arange(len(freqs)) - 4.0
 	expected_dB = [(val - 0.5, val + 0.5) for val in expected_vals]
 
@@ -71,27 +93,127 @@ def test(verbose=False):
 			linear=True
 		)
 	]
+
+	freqs = np.array([10., 31.6, 100., 316., 1000., 3162., 10000.]) / 44100.
+
+	expected_vals = -5.0 * np.arange(len(freqs))
+	expected_dB = [(val - 0.5, val + 0.5) for val in expected_vals]
+	expected_dB[0] = (-3., 0.)
+
+	tests += [
+		ProcessorUnitTest(
+			"PinkFilter(44.1 kHz)",
+			lambda: PinkFilter(44100.),
+			freqs_to_test=freqs,
+			expected_freq_response_range_dB=expected_dB,
+			expected_phase_response_range_degrees=None,
+			deterministic=True,
+			linear=True
+		),
+		ProcessorUnitTest(
+			"PinkFilter(44.1 kHz, sqrt2)",
+			lambda: PinkFilter(44100., pole_zero_ratio=sqrt2),
+			freqs_to_test=freqs,
+			expected_freq_response_range_dB=expected_dB,
+			expected_phase_response_range_degrees=None,
+			deterministic=True,
+			linear=True
+		),
+	]
+
+	freqs = np.array([10., 31.6, 100., 316., 1000., 3162., 10000., 31623.]) / 96000.
+
+	expected_vals = -5.0 * np.arange(len(freqs))
+	expected_dB = [(val - 0.5, val + 0.5) for val in expected_vals]
+	expected_dB[0] = (-3., 0.)
+
+	tests += [
+		ProcessorUnitTest(
+			"PinkFilter(96 kHz)",
+			lambda: PinkFilter(96000.),
+			freqs_to_test=freqs,
+			expected_freq_response_range_dB=expected_dB,
+			expected_phase_response_range_degrees=None,
+			deterministic=True,
+			linear=True
+		),
+		ProcessorUnitTest(
+			"PinkFilter(96 kHz, sqrt2)",
+			lambda: PinkFilter(96000., pole_zero_ratio=sqrt2),
+			freqs_to_test=freqs,
+			expected_freq_response_range_dB=expected_dB,
+			expected_phase_response_range_degrees=None,
+			deterministic=True,
+			linear=True
+		),
+		ProcessorUnitTest(
+			"PinkFilter(96 kHz, 1.1)",
+			lambda: PinkFilter(96000., pole_zero_ratio=1.1),
+			freqs_to_test=freqs,
+			expected_freq_response_range_dB=expected_dB,
+			expected_phase_response_range_degrees=None,
+			deterministic=True,
+			linear=True
+		),
+		ProcessorUnitTest(
+			"PinkFilter(96 kHz, 4)",
+			lambda: PinkFilter(96000., pole_zero_ratio=4),
+			freqs_to_test=freqs,
+			expected_freq_response_range_dB=expected_dB,
+			expected_phase_response_range_degrees=None,
+			deterministic=True,
+			linear=True
+		),
+	]
+
 	return run_unit_tests(tests, verbose=verbose)
 
 
 def plot(args):
 	from matplotlib import pyplot as plt
 	import numpy as np
+	from math import sqrt
 
 	from utils.plot_utils import plot_freq_resp
 
+	sqrt2 = sqrt(2.0)
 	sample_rate = 48000.
 
-	freqs = np.logspace(np.log10(1.0), np.log10(20000.0), 32, base=10)
+	def setup_plot():
+		fig, ax = plt.subplots(2, 1)
+		x = [10, 10000]
+		y = [0, -30]  # 10 dB/decade (often cited as 3 dB/octave, which is very close but not quite exact)
+		ax[0].plot(x, y, 'g-')
+		ax[1].axhline(-45, color='g')
+		return fig, ax
 
-	plot_freq_resp(BasicPinkFilter, None, None, freqs, sample_rate, n_samp=48000)
-	plot_freq_resp(PinkFilter, None, dict(sample_rate=48000), freqs, 48000, n_samp=48000)
-	plot_freq_resp(PinkFilter, None, dict(sample_rate=96000), freqs, 96000, n_samp=96000)
+	print('Processing & plotting...')
 
-	freqs, gains = PinkFilter._calc_individual_filters(sample_rate)
-	args_list = [dict(wc=freq/sample_rate, gain=gain) for freq, gain in zip(freqs, gains)]
-	plot_freq_resp(BasicOnePole, None, args_list, freqs, 48000, n_samp=48000, freq_args=['wc'])
-	plt.title('Individual filters in PinkFilter')
+	plot_freqs = np.logspace(np.log10(1.0), np.log10(20000.0), 64, base=10)
+
+	_, axes = setup_plot()
+	plot_freq_resp(BasicPinkFilter, None, None, plot_freqs, sample_rate, n_samp=48000, phase=True, axes=axes)
+
+	_, axes = setup_plot()
+	plot_freq_resp(PinkFilter, None, dict(sample_rate=48000), plot_freqs, 48000, n_samp=48000, phase=True, axes=axes)
+
+	_, axes = setup_plot()
+	plot_freq_resp(PinkFilter, None, dict(sample_rate=96000), plot_freqs, 96000, n_samp=96000, phase=True, axes=axes)
+
+	_, axes = setup_plot()
+	plot_freq_resp(PinkFilter, None, dict(sample_rate=96000, pole_zero_ratio=sqrt2), plot_freqs, 96000, n_samp=96000, phase=True, axes=axes)
+
+	_, axes = setup_plot()
+	plot_freq_resp(PinkFilter, None, dict(sample_rate=96000, pole_zero_ratio=1.1), plot_freqs, 96000, n_samp=96000, phase=True, axes=axes)
+
+	_, axes = setup_plot()
+	plot_freq_resp(PinkFilter, None, dict(sample_rate=96000, pole_zero_ratio=4), plot_freqs, 96000, n_samp=96000, phase=True, axes=axes)
+
+	for pole_zero_ratio in [2, sqrt2, 4]:
+		cutoff_freqs, gains = PinkFilter._calc_individual_filters(sample_rate, pole_zero_ratio=pole_zero_ratio)
+		args_list = [dict(wc=freq/sample_rate, gain=gain) for freq, gain in zip(cutoff_freqs, gains)]
+		plot_freq_resp(BasicOnePole, None, args_list, plot_freqs, 48000, n_samp=48000, freq_args=['wc'])
+		plt.title('Individual filters in PinkFilter, pole-zero ratio %g' % pole_zero_ratio)
 
 	print('Showing plots')
 	plt.show()
